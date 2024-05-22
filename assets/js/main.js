a05511382bdbbb6c01f340aed0eb3d570b98fef5
fsantosandrade <160565775+fsantosandrade@users.noreply.github.com>
2024-05-22 18:50:36 -0300

    update json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
+function updateProfileInfo (profileData) {
+    const photo = document.querySelector("#profile.photo")
+    photo.src = profileData.photo
+
 (async function () {
     const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
     console.log(profileData)
 })()
