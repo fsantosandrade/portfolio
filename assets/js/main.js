061b6e2d1fdb4105438b3670245f307a60d9d91e
fsantosandrade <160565775+fsantosandrade@users.noreply.github.com>
2024-05-22 19:14:46 -0300

    preenchendo infos cabeçalho

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,21 @@ function updateProfileInfo (profileData) {
     const photo = document.getElementById("profile.photo")
     photo.src = profileData.photo
     photo.alt = profileData.name
+
+    const name = document.getElementById("profile.name")
+    name.innerHTML = profileData.name
+
+    const job = document.getElementById("profile.job")
+    job.innerHTML = profileData.job
+
+    const loc = document.getElementById("profile.location")
+    loc.innerHTML = profileData.location
+
+    const phone = document.getElementById("profile.phone")
+    phone.innerHTML = profileData.phone
+
+    const email = document.getElementById("profile.email")
+    email.innerHTML = profileData.email
 }
 
 (async function () {
